fd152e56d226e14afab204953d01af5e36e0b31c
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-21 12:57:25 -0600

    edad.c

--- edad.c
+++ edad.c
@@ -5,4 +5,11 @@ int main (){
     int edad, año=2003, dia=30, mes = 3;
     clock_t start, end;
     double cpu_time;
+     edad = 2024 - año;
+    int dias = edad * 365;
+    dias = dias - 365;
+    int dias2= mes - 1 * 30 + mes ;
+    dias = dias2 + dias;
+    int meses = edad * 12;
+    meses = meses - 12 + mes;
 }
