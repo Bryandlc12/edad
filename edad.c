e0700c7f77f14b69c416a5573874ee6ac04df443
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-21 12:57:56 -0600

    edad.c

--- edad.c
+++ edad.c
@@ -5,6 +5,7 @@ int main (){
     int edad, año=2003, dia=30, mes = 3;
     clock_t start, end;
     double cpu_time;
+    
      edad = 2024 - año;
     int dias = edad * 365;
     dias = dias - 365;
@@ -12,4 +13,10 @@ int main (){
     dias = dias2 + dias;
     int meses = edad * 12;
     meses = meses - 12 + mes;
+    
+    printf("años: %d meses: %d dias: %d\n", edad, meses, dias);
+    end = clock();
+    cpu_time = ((double) (end - start)) / CLOCKS_PER_SEC; 
+    printf("El código tomó %f segundos en ejecutarse.\n", cpu_time);
+    return 0;
 }
